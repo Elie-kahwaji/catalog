456bd13c936ba6b3c4d1bcb1b26d3a1be5864f6e
elie kahwaji <elie.e.kahwaji@gmail.com>
2022-01-23 00:38:29 +0200

    grafana and docker config

--- src/main/java/com/baseware/eshop/catalog/configuration/MonitoringAndLoggingConfig.java
+++ src/main/java/com/baseware/eshop/catalog/configuration/MonitoringAndLoggingConfig.java
@@ -3,6 +3,7 @@ package com.baseware.eshop.catalog.configuration;
 import com.baseware.eshop.catalog.core.logging.LoggingConfig;
 import com.baseware.eshop.catalog.core.monitoring.PerformanceMonitoringConfig;
 import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,8 +12,12 @@ import org.springframework.context.annotation.Import;
 @Configuration
 @Import({LoggingConfig.class, PerformanceMonitoringConfig.class})
 public class MonitoringAndLoggingConfig {
+
+  @Value("${spring.application.name}")
+  private String appName;
+
   @Bean
   MeterRegistryCustomizer<MeterRegistry> metricsCommonTags() {
-    return registry -> registry.config().commonTags("application", "catalog", "instanceId", "catalog_1");
+    return registry -> registry.config().commonTags("application", appName);
   }
 }
