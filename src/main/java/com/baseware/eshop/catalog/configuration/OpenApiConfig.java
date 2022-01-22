1bf158a35c7b1609c1145ebd092185c7a0b867d6
elie kahwaji <elie.e.kahwaji@gmail.com>
2022-01-22 19:33:29 +0200

    fixing local machine mapping for docker and add redirection for open api

--- src/main/java/com/baseware/eshop/catalog/configuration/OpenApiConfig.java
+++ src/main/java/com/baseware/eshop/catalog/configuration/OpenApiConfig.java
@@ -5,9 +5,11 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class OpenApiConfig {
+public class OpenApiConfig implements WebMvcConfigurer {
  
     @Bean
     public OpenAPI openAPI() {
@@ -16,5 +18,12 @@ public class OpenApiConfig {
                 .info(new Info()
                           .title("Catalog Application API")
                           .description("This is the product catalog based on a microservice for the eshop solution."));
+    }
+
+    @Override
+    public void addViewControllers(final ViewControllerRegistry registry) {
+        registry.addRedirectViewController("/", "/swagger-ui.html");
+
+        registry.addRedirectViewController("/swagger-ui", "/swagger-ui.html");
     }
 }
